460e50e72d135ee24a7f9be80c806e5c94aa0aff
jiminyoon1 <dbswlals16@gmail.com>
2024-03-18 17:48:04 +0900

    scriptprintpractice

--- Assets/W03_input/NewBehaviourScript.cs
+++ Assets/W03_input/NewBehaviourScript.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,7 +7,9 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("윤지민");
+        print("한국화과");
+        print("2019101648");
     }
 
     // Update is called once per frame
